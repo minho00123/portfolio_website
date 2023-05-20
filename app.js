b1d69b463db3e2b27329dca3efe9e867069433a6
Min Ho Jang <mino@Mins-MacBook-Air.local>
2023-05-20 11:16:53 +0900

    feat: add Marquee Section

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// HAMBURGER
 const hamburger = document.querySelector(".hamburger");
 const navUl = document.querySelector(".nav-ul");
 
@@ -12,3 +13,17 @@ document.querySelectorAll(".nav-a").forEach((n) =>
     navUl.classList.remove("active");
   })
 );
+
+// SCROLL
+const root = document.documentElement;
+const marqueElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marque-elements-displayed"
+);
+
+const marqueContent = document.querySelector(".marque-content");
+
+root.style.setProperty("--marque-elements", marqueContent.children.length);
+
+for (let i = 0; i < marqueElementsDisplayed; i++) {
+  marqueContent.appendChild(marqueContent.children[i].cloneNode(true));
+}
